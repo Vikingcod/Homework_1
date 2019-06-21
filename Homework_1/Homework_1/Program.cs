c713e5d01def5d805dce5245136d934ec2829152
BSzver <BSzver@gmail.com>
2019-06-21 21:22:49 +0300

    Создал несколько переменных для программы высчитавающей индекс массы тела

---
+++ Homework_1/Homework_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname;
+            string lastname;
+            string age;
+        }
+    }
+}
